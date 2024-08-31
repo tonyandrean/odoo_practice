a57738e1b9ed3da97ae263f6b1ad8c4ec0355e89
tony <tony@tilabs.id>
2024-09-01 02:33:06 +0700

    Continue later

--- tr_library/models/rental_model.py
+++ tr_library/models/rental_model.py
@@ -5,11 +5,11 @@ from odoo import models, fields, api
 class RentalModel(models.Model):
     _name = 'rental.model'
     _description = 'Rental Model Library'
-    _rec_name = 'customer'
+    _rec_name = 'customer_id'
 
 
 
-    customer = fields.Many2one('res.partner', string='Customer')
+    customer_id = fields.Many2one('res.partner', string='Customer')
     book_id = fields.Many2one('book.model', string='Book Reference')
     longitude = fields.Float(string='Longitude')
     latitude = fields.Float(string='Latittude')
